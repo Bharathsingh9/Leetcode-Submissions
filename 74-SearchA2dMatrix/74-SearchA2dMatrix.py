5896044238c3b964b79566f5b4614e9afff3606a
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:48:09 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.py
@@ -0,0 +1,18 @@
+# Last updated: 7/3/2026, 12:48:06 PM
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        m = len(matrix[0])
+        low = 0
+        high = n * m - 1
+        while low <= high:
+            mid = (low + high) // 2
+            row = mid // m
+            col = mid % m
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
