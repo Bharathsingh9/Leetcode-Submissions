c253a87ef4ebce77b2b6dd08e64469c07106e429
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:47:02 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,19 @@
+# Last updated: 7/3/2026, 12:46:59 PM
+class Solution(object):
+    def search(self, nums, target):
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                high=mid-1
+            else:
+                low=mid+1
+        return -1
+            
+        
+        
+
+        
