f4a56e73d6207088b3cf930f02cf8794a62e8547
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:48:10 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.py
@@ -0,0 +1,19 @@
+# Last updated: 7/3/2026, 12:48:08 PM
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        low,mid = 0, 0
+        high = len(nums)-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low],nums[mid] = nums[mid], nums[low]
+                mid+=1
+                low+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+
+
+      
+        
