fbef1b212a60e8c91170d7c374a2ffd2443dc86e
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:48:47 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -0,0 +1,15 @@
+# Last updated: 7/3/2026, 12:48:44 PM
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def dfs(openP, closeP, s):
+            if openP == closeP and openP + closeP == n * 2:
+                res.append(s)
+                return
+            if openP < n:
+                dfs(openP + 1, closeP, s + "(")
+            
+            if closeP < openP:
+                dfs(openP, closeP + 1, s + ")")
+        dfs(0, 0, "")
+        return res
