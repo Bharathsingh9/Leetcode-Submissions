5028ca8205156ec587d694d9807ccef877e46a52
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:48:06 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.py
@@ -0,0 +1,26 @@
+# Last updated: 7/3/2026, 12:48:03 PM
+class Solution(object):
+    def search(self, nums, target):
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return True
+            if nums[low] == nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+            if nums[low]<=nums[mid]:
+                if nums[low]<=target<nums[mid]:
+                    high=mid-1
+                else:
+                    low=mid+1
+            else:
+                if nums[mid]<target<=nums[high]:
+                    low=mid+1
+                else:
+                    high=mid-1
+        return False
+
+        
