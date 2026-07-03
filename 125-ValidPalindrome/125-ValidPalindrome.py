ee9accb6513ac0661abe2c0e37e4e8d1688bafb3
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:47:54 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,18 @@
+# Last updated: 7/3/2026, 12:47:52 PM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+        while left < right:
+            if not s[left].isalnum():
+                left+=1
+            elif not s[right].isalnum():
+                right-=1
+            elif s[left].lower()!=s[right].lower():
+                return False
+            else:
+                left+=1
+                right-=1
+        return True
+            
+        
