b6d66aba1f4b69b89693cea8aca765c053a2b642
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:46:39 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,5 @@
+-- Last updated: 7/3/2026, 12:46:36 PM
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
+GROUP BY activity_date;
