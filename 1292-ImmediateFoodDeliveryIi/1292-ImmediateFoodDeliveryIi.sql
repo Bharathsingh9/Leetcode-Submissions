4f81401838843438e33d575f1aded6011711b61a
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:46:35 +0530

    Create 1292-ImmediateFoodDeliveryIi

---
+++ 1292-ImmediateFoodDeliveryIi/1292-ImmediateFoodDeliveryIi.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/3/2026, 12:46:33 PM
+Select 
+    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+);
