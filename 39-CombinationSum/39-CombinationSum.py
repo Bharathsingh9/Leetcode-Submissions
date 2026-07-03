19ff0204ccddd7b1e611eae9041172dd96ce4f12
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-03 12:48:31 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.py
@@ -0,0 +1,21 @@
+# Last updated: 7/3/2026, 12:48:28 PM
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        res = []
+        
+        def dfs(i, curr, path: list):
+            if curr == 0:
+                res.append(path[:])
+                return
+            
+            if (i == n and curr != 0) or curr < 0:
+                return
+            
+            path.append(candidates[i])
+            dfs(i, curr - candidates[i], path)
+            path.pop()
+            dfs(i+1, curr, path)
+        
+        dfs(0, target, [])
+        return res
