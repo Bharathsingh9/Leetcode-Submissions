ec7bca16a65c515fdd1de03e4f57908011e29a38
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-26 10:23:20 +0530

    Create 628-MaximumProductofThreeNumbers

---
+++ 628-MaximumProductofThreeNumbers/version/python/628-MaximumProductofThreeNumbers_20260726_102321.py
@@ -0,0 +1,7 @@
+# Last updated: 7/26/2026, 10:23:21 AM
+1class Solution:
+2    def maximumProduct(self, nums: List[int]) -> int:
+3        nums.sort()
+4        res = max(nums[-1]*nums[-2]*nums[-3], nums[0]*nums[1]*nums[-1])
+5        return res
+6        
