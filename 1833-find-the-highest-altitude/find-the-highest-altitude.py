78d09f49deb652ce1763e7444230524203e7d7c6
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-26 10:41:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (49.49%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        for i in range(len(gain)-1):
+            gain[i+1] = gain[i]+gain[i+1]
+        if max(gain)<0:
+            return 0
+        else:
+            return max(gain)
