ed965a6249d91a83880849b32cc7d6da39a98a15
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-07-28 19:06:37 +0530

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/version/python/3517-SmallestPalindromicRearrangementI_20260728_190635.py
@@ -0,0 +1,15 @@
+# Last updated: 7/28/2026, 7:06:35 PM
+1class Solution:
+2    def smallestPalindrome(self, s: str) -> str:
+3        if len(s) == 1:
+4            return s
+5        n = len(s)
+6        mid = n//2
+7        if n%2 == 0:
+8            c = sorted(s[:mid])
+9            d = c[::-1]
+10            return "".join(c+d)
+11        elif n%2 == 1:
+12            f = sorted(s[:mid])
+13            g = f[::-1]
+14            return "".join(f+list(s[mid])+g)
