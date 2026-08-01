acea54e1b414ff71ae4b49bb8aa233468f77850b
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-08-01 21:31:50 +0530

    Create

---
+++ problem-1785600112012/version/python/problem-1785600112012_20260801_213152.py
@@ -0,0 +1,16 @@
+# Last updated: 8/1/2026, 9:31:52 PM
+1class Solution:
+2    def countValidPrefixes(self, s: str) -> int:
+3        i = 0
+4        j = 0
+5        r = 0
+6        for k in s:
+7            if k == "0":
+8                i+=1
+9            else:
+10                j+=1
+11
+12            if abs(i-j)<=1:
+13                r+=1
+14        return r
+15        
