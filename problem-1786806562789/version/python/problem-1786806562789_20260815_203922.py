ce3edbab172155ed7c411c7efe14db214c2d143c
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-08-15 20:39:25 +0530

    Create

---
+++ problem-1786806562789/version/python/problem-1786806562789_20260815_203922.py
@@ -0,0 +1,13 @@
+# Last updated: 8/15/2026, 8:39:22 PM
+1class Solution:
+2    def minOperations(self, s: str) -> int:
+3        j = len(s)
+4        res = float('inf')
+5        for i in range(j):
+6            c = i
+7            for k in range(j//2):
+8                a = ord(s[(k+i)%j])-ord('a')
+9                b = ord(s[(j-1-k+i)%j]) - ord('a')
+10                c+=min((a-b) % 26,(b-a) % 26)
+11            res = min(res,c)
+12        return res
