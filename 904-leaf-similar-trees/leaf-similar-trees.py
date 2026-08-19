3540d9123b749da99b4bb8beb53f1d800e4d87c2
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-08-19 21:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (45.33%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -6,22 +6,17 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        res = []
-        ans = []
-        def dfs1(root1):
-            if not root1:
-                return 
-            if not root1.left and not root1.right:
-                res.append(root1.val)
-            dfs1(root1.left)
-            dfs1(root1.right)
-        dfs1(root1)
-        def dfs2(root2):
-            if not root2:
-                return 
-            if not root2.left and not root2.right:
-                ans.append(root2.val)
-            dfs2(root2.left)
-            dfs2(root2.right)
-        dfs2(root2)
-        return ans == res
+        def get_leaves(root):
+            leaves = []
+            def dfs(node):
+                if not node:
+                    return
+                if not node.left and not node.right:
+                    leaves.append(node.val)
+                    return
+                dfs(node.left)
+                dfs(node.right)
+            dfs(root)
+            return leaves
+        return get_leaves(root1) == get_leaves(root2)
+        
