20a419bbc3fe1ea7f66f87f86450c52989bed70f
Bharathsingh9 <bharathpalthyavath@gmail.com>
2026-08-21 23:32:39 +0530

    Create 515-FindLargestValueinEachTreeRow

---
+++ 515-FindLargestValueinEachTreeRow/version/python/515-FindLargestValueinEachTreeRow_20260821_233238.py
@@ -0,0 +1,29 @@
+# Last updated: 8/21/2026, 11:32:38 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7from collections import deque
+8class Solution:
+9    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+10        if not root:
+11            return []
+12        queue = deque([root])
+13        res = []
+14        while queue:
+15            l = []
+16            size = len(queue)
+17            for i in range(size):
+18                n = queue.popleft()
+19                l.append(n.val)
+20                if n.left:
+21                    queue.append(n.left)
+22                if n.right:
+23                    queue.append(n.right)
+24            res.append(l)
+25        a = []
+26        for j in res:
+27            a.append(max(j))
+28        return a
